cdd6e695628cd28f95d8de4e6324f69b3e05b97c
Nícolas Sanchez Soares <nico_sansoares@hotmail.com>
2024-03-21 15:49:43 -0300

    Confirmando mudanças, quebrou outras funcionalidades

--- Confirm.xaml.cs
+++ Confirm.xaml.cs
@@ -5,55 +5,81 @@ namespace JogoGourmet
     public partial class Confirm : Window
     {
         private Dictionary<string, List<string>> perguntasD;
-        private string[] keys; // Armazena as chaves do dicionário
-        private int indicePerguntaAtual = 0; // Índice da chave atual no dicionário
-        private int indicePerguntaFilhoAtual = 0; // Índice da pergunta filho atual na lista associada à chave atual
-        readonly MainWindow parent;
+        private List<string> valoresAssociados;
+        private int indiceAtual = 0;
+        private MainWindow parent;
+        private bool mostrandoValores = false;
 
-        public Confirm(MainWindow caller, Dictionary<string, List<string>> perguntas, string perguntaAtual = "", bool verificarVinculo = false)
+        public Confirm(MainWindow caller, Dictionary<string, List<string>> perguntas)
         {
             InitializeComponent();
             perguntasD = perguntas;
-            keys = perguntas.Keys.ToArray();
             parent = caller;
+            InicializarValoresAssociados();
+            MostrarPerguntaAtual();
+        }
 
-            if (verificarVinculo)
+        private void InicializarValoresAssociados()
+        {
+            valoresAssociados = new List<string>();
+        }
+
+        private void MostrarPerguntaAtual()
+        {
+            if (indiceAtual < perguntasD.Count)
             {
-                txtPergunta.Text = $"O prato que pensou é {perguntas[perguntaAtual][0]}?";
+                string pergunta = perguntasD.Keys.ElementAt(indiceAtual);
+
+                // Se estivermos mostrando os valores associados, exibimos os valores
+                if (mostrandoValores)
+                {
+                    txtPergunta.Text = valoresAssociados.Any() ? valoresAssociados.First() : "Não há pratos associados a esta pergunta.";
+                }
+                else
+                {
+                    // Mostra a pergunta original
+                    txtPergunta.Text = pergunta;
+                }
             }
             else
             {
-                txtPergunta.Text = keys[0];
+                // Todas as perguntas já foram mostradas
+                CriaPrato adicionaForm = new CriaPrato(parent, perguntasD);
+                this.Close();
+                adicionaForm.Show();
             }
         }
+
         private void btnSim_Click(object sender, RoutedEventArgs e)
         {
-            VerificarVinculo();
+            if (!mostrandoValores && perguntasD.ContainsKey(txtPergunta.Text) && perguntasD[txtPergunta.Text].Any())
+            {
+                // Se houver valores associados à pergunta, mostramos os valores
+                mostrandoValores = true;
+                valoresAssociados = perguntasD[txtPergunta.Text];
+            }
+            else
+            {
+                // Senão, verificamos o vínculo
+                VerificarVinculo();
+            }
+            MostrarPerguntaAtual();
         }
 
         private void btnNao_Click(object sender, RoutedEventArgs e)
         {
-            if (indicePerguntaFilhoAtual < perguntasD[keys[indicePerguntaAtual]].Count - 1)
+            if (mostrandoValores && valoresAssociados.Count > 1)
             {
-                indicePerguntaFilhoAtual++;
-                txtPergunta.Text = $"O prato que pensou é {perguntasD[keys[indicePerguntaAtual]][indicePerguntaFilhoAtual]}?";
+                // Se houver mais valores associados, mostramos o próximo valor
+                valoresAssociados.RemoveAt(0);
             }
             else
             {
-                indicePerguntaAtual++;
-
-                if (indicePerguntaAtual < keys.Length)
-                {
-                    indicePerguntaFilhoAtual = 0;
-                    txtPergunta.Text = keys[indicePerguntaAtual];
-                }
-                else
-                {
-                    CriaPrato adicionaForm = new(parent, perguntasD);
-                    this.Close();
-                    adicionaForm.Show();
-                }
+                // Senão, passamos para a próxima pergunta
+                mostrandoValores = false;
+                indiceAtual++;
             }
+            MostrarPerguntaAtual();
         }
 
         #region VerificarVinculo
@@ -62,7 +88,7 @@ namespace JogoGourmet
             if (VerificarVinculoExistente(txtPergunta.Text))
             {
                 this.Hide();
-                Confirm perguntaForm = new(parent, perguntasD, txtPergunta.Text, true);
+                Confirm perguntaForm = new(parent, perguntasD);
                 perguntaForm.Show();
             }
             else
